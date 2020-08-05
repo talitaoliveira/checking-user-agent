53b5ccbda55ef17fa298558f226c1d67b0c1f155
Talita Oliveira <litaa.oliveira@gmail.com>
2020-08-05 12:15:36 -0300

    Navigation on header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return <h1>Header</h1>
+    return <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+    </div>
 }
 
 export default Header;
