41da87e35488259cf5c4c0f82aa0d82f4084be7e
Talita Oliveira <litaa.oliveira@gmail.com>
2020-08-05 12:18:36 -0300

    Updates

---
+++ precache-manifest.29608d487c5dd73b38a0b90d04185832.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d7b355b80c4e48630c1d124b4c061a",
+    "url": "/dynamic-style/index.html"
+  },
+  {
+    "revision": "1163765450d06f0a73ca",
+    "url": "/dynamic-style/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "022c6ab4f79e17ee85b4",
+    "url": "/dynamic-style/static/js/2.8a821478.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dynamic-style/static/js/2.8a821478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1163765450d06f0a73ca",
+    "url": "/dynamic-style/static/js/main.78f56b0f.chunk.js"
+  },
+  {
+    "revision": "45dcbb30bc72bf2efce4",
+    "url": "/dynamic-style/static/js/runtime-main.6e92d702.js"
+  }
+]);
