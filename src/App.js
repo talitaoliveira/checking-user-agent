bc60507c6addacf8be06fe4c8b58e908c7c1a504
Talita Oliveira <litaa.oliveira@gmail.com>
2020-08-05 11:31:35 -0300

    Create components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header/Header';
+import Menu from './components/Menu/Menu';
+import Main from './components/Pages/Main/Main';
+import About from './components/Pages/About/About';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <Menu/>
+      <Main/>
+      <About/>
     </div>
   );
 }
