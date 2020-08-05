47e558e4e93800a7927ff048e789e332cef1b165
Talita Oliveira <litaa.oliveira@gmail.com>
2020-08-05 12:15:43 -0300

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React from 'react';
-import Header from './components/Header/Header';
-import Menu from './components/Menu/Menu';
-import Main from './components/Pages/Main/Main';
-import About from './components/Pages/About/About';
+import React from "react";
+import Header from "./components/Header/Header";
+import Users from "./components/Pages/Users/Users";
+import Home from "./components/Pages/Home/Home";
+import About from "./components/Pages/About/About";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <Header/>
-      <Menu/>
-      <Main/>
-      <About/>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
